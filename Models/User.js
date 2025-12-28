2fbac5ea9c7528d98c61c130ab12740a63b50e23
Noore <official.noore.shop@gmail.com>
2025-12-28 07:47:43 -0800

    First Commit

---
+++ Models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  _Id: { type: String, required: true,  },
+  Name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  imageUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  cartItems:{ type: Object, default: {}}
+}, {minimize: false});
+
+const User = mongoose.models.User || mongoose.model('user', userSchema);
+export default User;
