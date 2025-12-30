e96acc5760bb344c23c4122aec9d1bb3f5705d3e
Noore <official.noore.shop@gmail.com>
2025-12-30 03:37:07 -0800

    Function

---
+++ app/api/messages/route.js
@@ -0,0 +1,1 @@
+// Placeholder file to resolve missing file error
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "!./app/api/messages/**/*", // Exclude the messages directory
   ],
   theme: {
     extend: {
